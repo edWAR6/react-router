2f50d6d663c37d93e0249183eea2ea92fce1ec3d
Eduardo Oviedo Blanco <eduardo.oviedo@gmail.com>
2019-11-28 20:13:44 -0600

    React router ready and working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { Link, Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+
+import About from './About';
+import Home from './Home';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <h2>Welcome to React Router Tutorial</h2>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <ul className="navbar-nav mr-auto">
+            <li><Link to={'/'} className="nav-link"> Home </Link></li>
+            <li><Link to={'/about'} className="nav-link">About</Link></li>
+          </ul>
+        </nav>
+        <hr />
+        <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/about' component={About} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
