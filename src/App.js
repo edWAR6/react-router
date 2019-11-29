ea7b38e1e631747d4d8713d3a956c0279fa72d7d
Eduardo Oviedo Blanco <eduardo.oviedo@gmail.com>
2019-11-28 21:07:30 -0600

    Practice ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import './App.css';
+import { Link, Route, Switch } from 'react-router-dom';
 
+import About from './About';
+import Home from './Home';
 import React from 'react';
-import logo from './logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>React Router 🚦</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to={'/'}>Home</Link>
+            </li>
+            <li>
+              <Link to={'/about'}>About</Link>             
+            </li>
+          </ul>
+        </nav>
       </header>
+      <section>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+        </Switch>
+      </section>
     </div>
   );
 }
